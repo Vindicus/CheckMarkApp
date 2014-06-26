b2ee0a0e728658b4abe0322ccd7cca222e13ac9e
Vindicus <pyang1016@gmail.com>
2014-06-26 02:41:26 +0000

    allow users to update their account information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:first_name, :last_name, :phone_number, :email, :password, :password_confirmation)
     end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :last_name, :phone_number, :email, :password, :password_confirmation,:current_password)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   validates :first_name, :last_name, presence: true
-  validates :phone_number, format:{with: /[0-9]{3}-[0-9]{3}-[0-9]{4}/,
+  validates :phone_number, presence: true, format:{with: /[0-9]{3}-[0-9]{3}-[0-9]{4}/,
     message: "Your phone number must be in xxx-xxx-xxxx format."  }
+  
 end
