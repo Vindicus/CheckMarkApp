4980c73498c8946a821acfac2e87bbea3fceb0d4
Vindicus <pyang1016@gmail.com>
2014-07-29 05:10:35 +0000

    store DRY twilio code into helper file

--- app/workers/phone_worker.rb
+++ app/workers/phone_worker.rb
@@ -1,18 +1,16 @@
 require 'twilio-ruby'
 class PhoneWorker
+  include TwilioHelper
   include Sidekiq::Worker
   
   def perform(user,appointment)
-    account_sid = ENV['ACCOUNT_SID']
-    auth_token = ENV['AUTH_TOKEN']
-    @client = Twilio::REST::Client.new account_sid, auth_token
     user_find=User.find(user)
     appointment_find=Appointment.find(appointment)
     appointment_title=appointment_find.title.gsub!(/\s+/,"%20")
-       @call = @client.account.calls.create(
+       @call = @@Client.account.calls.create(
     :from => '+12096473083',
     :to => "+#{user_find.phone_number}",
-         :url => "http://www.todo-122362.usw1-2.nitrousbox.com/twilio/voice?title=#{appointment_title}",
+    :url => "http://www.todo-122362.usw1-2.nitrousbox.com/twilio/voice?title=#{appointment_title}",
 )
   end
   
--- app/workers/sms_worker.rb
+++ app/workers/sms_worker.rb
@@ -1,20 +1,17 @@
 require 'twilio-ruby'
 class SmsWorker
+  include TwilioHelper
   include Sidekiq::Worker
 
   def perform(user,appointment)
-    account_sid = ENV['ACCOUNT_SID']
-    auth_token = ENV['AUTH_TOKEN']
-    @client = Twilio::REST::Client.new account_sid, auth_token
     user_find=User.find(user)
     appointment_find=Appointment.find(appointment)
-    @client.account.messages.create(
+    @@Client.account.messages.create(
       :from => '+12096473083',
       :to => "+#{user_find.phone_number}",
       :body => "hello, #{user_find.first_name}. You have an appointment called: #{appointment_find.title} coming up. Please see CheckMark for details. Thank you, good bye."
 )
   end
-  
+
   
 end
-
