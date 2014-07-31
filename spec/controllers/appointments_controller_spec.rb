1e5009ed59c134b896ab628dc86f8bcb97401f81
Vindicus <pyang1016@gmail.com>
2014-07-30 20:06:01 -0700

    Fixing indentations

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -1,32 +1,35 @@
 require 'rails_helper'
 
 describe AppointmentsController do
- before (:each) do
-  @user = User.create!({
-    :first_name => "test",
-    :last_name => "test",
-    :phone_number => "000-000-0000",
-    :email => 'user@test.com',
-    :password => 'pleaseee',
-    :password_confirmation => 'pleaseee'
+  before (:each) do
+    @user = User.create!({
+      :first_name => "test",
+      :last_name => "test",
+      :phone_number => "000-000-0000",
+      :email => 'user@test.com',
+      :password => 'pleaseee',
+      :password_confirmation => 'pleaseee'
     })
-  sign_in @user
-end
+    sign_in @user
+  end
+  
   describe "GET #index" do
     subject {get :index}
     it"should GET index action successfully" do
       expect(response.status).to eq(200)
     end
+    
     it "should render GET #index view" do
       expect(subject).to render_template("index")
     end
+    
     xit "populates user's appointments" do
       appointment=FactoryGirl.create(:appointment)
       get :index
       expect(assigns(:appointments)).to eq([appointment])
     end
-    
   end
+  
   describe "GET #new" do
     subject {get :new}
     it"should GET new action successfully" do
@@ -36,17 +39,20 @@ end
       expect(subject).to render_template("new")
     end
   end
+  
   describe "POST #create" do
     xit "creates a new appointment" do
       post :create
       expect{attributes_for(appointment)}.to change(Appointment,:count).by(1)
     end
   end
+  
   describe "GET #edit" do
     it "should GET edit action successfully" do
       expect(response.status).to eq(200)
+    end
   end
-  end
+  
   describe "PATCH #update" do
     let(:appointment) {FactoryGirl.build(:appointment, :title => "Dentist")}
     xit "updates the appointment attributes" do
@@ -55,12 +61,11 @@ end
       expect(appointment.title).to equal("Dentist")
     end
   end
+  
   describe "DELETE #destroy" do
     xit "deletes an appointment" do
       subject {FactoryGirl.create(:appointment, title: "birthday", description: "party", location: "home" ,date: "2013-06-07", time: "2000-01-01 09:33:00 UTC" )}
       expect{delete :destroy, id: subject}.to change(Appointment,:count).by(-1)
     end
   end
-  
-  
 end
