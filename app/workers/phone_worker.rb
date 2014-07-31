959dae7f0356c9ede8cb58a22543634859e4687f
Vindicus <pyang1016@gmail.com>
2014-07-30 19:56:10 -0700

    Removed extra lines

--- app/workers/phone_worker.rb
+++ app/workers/phone_worker.rb
@@ -7,16 +7,11 @@ class PhoneWorker
     user_find=User.find(user)
     appointment_find=Appointment.find(appointment)
     appointment_title=appointment_find.title.gsub!(/\s+/,"%20")
-       @call = @@Client.account.calls.create(
+    
+    @call = @@Client.account.calls.create(
     :from => '+12096473083',
     :to => "+#{user_find.phone_number}",
     :url => "http://checkmark-vindicus.herokuapp.com/twilio/voice?title=#{appointment_title}",
-)
+    )
   end
-  
-  
-
 end
-
-
-  
