f95711849c4f512cc8b3414f4ba6fadb2de0b6d0
Vindicus <pyang1016@gmail.com>
2014-07-30 20:00:06 -0700

    fixed minor indentations

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,17 +1,19 @@
 if defined? Sidekiq
   redis_url = ENV['REDISTOGO_URL'] || ENV['REDIS_URL']
+  
   Sidekiq.configure_server do |config|
     config.redis = {
       url: redis_url,
       namespace: 'workers',
       size: 2
-      }
+    }
   end
+  
   Sidekiq.configure_client do |config|
     config.redis = {
       url: redis_url,
       namespace: 'workers',
       size: 1
-      }
+    }
   end
 end
