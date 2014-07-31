392bb44c9ce81b702626e943846471cd581c9d4b
Vindicus <pyang1016@gmail.com>
2014-07-30 19:58:20 -0700

    added comment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  #Using Sendgrid for mailing
   config.action_mailer.default_url_options = { host: 'localhost:3000' } 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method= :smtp
