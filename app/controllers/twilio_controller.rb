79f10ccb6f585a39880c00677ec2385e144a83db
Vindicus <pyang1016@gmail.com>
2014-07-30 19:20:35 -0700

    Added a comment

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -4,11 +4,14 @@ class TwilioController < ApplicationController
   after_filter :set_header
   skip_before_action :verify_authenticity_token
   
+  #Prompts the Twilio call to render a response based on this response
   def voice
     title=params[:title]
-      response = Twilio::TwiML::Response.new do |r|
-        r.Say "Hello, you have an appointment called #{title}. Please see CheckMark for details", :voice => 'alice'  
+    
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "Hello, you have an appointment called #{title}. Please see CheckMark for details", :voice => 'alice'  
     end
+    
     render_twiml response
-end
+  end
 end
