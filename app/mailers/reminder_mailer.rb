6e3684b97319e4a1f2252112e2e5ef813d22e388
Vindicus <pyang1016@gmail.com>
2014-07-30 19:22:23 -0700

    Added comment

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,9 +1,11 @@
 class ReminderMailer < ActionMailer::Base
-   default from: "do-not-reply@checkmark.com"
+  default from: "do-not-reply@checkmark.com"
   
+  #Sends an email to the user using Sendgrid
   def send_email_reminder(user, appointment)
-     @user=User.find(user)
-     @appointment=Appointment.find(appointment)
+    @user=User.find(user)
+    @appointment=Appointment.find(appointment)
+    
     mail(:to => @user.email, :subject => "Your appointment reminder" )
   end
 end
